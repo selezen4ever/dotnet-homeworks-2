2710c973f710eed825748adec773ae39c1b7e6f7
selezen4ever <apdasd@mail.ru>
2022-10-02 23:53:00 +0300

    complete Task-3

--- Homework3/Hw3.Tests/SingleInitializationSingleton.cs
+++ Homework3/Hw3.Tests/SingleInitializationSingleton.cs
@@ -5,12 +5,15 @@ namespace Hw3.Tests;
 
 public class SingleInitializationSingleton
 {
+    public const int DefaultDelay = 3_000;
+    
     private static readonly object Locker = new();
 
     private static volatile bool _isInitialized = false;
+    public static SingleInitializationSingleton Instance => _instance.Value;
 
-    public const int DefaultDelay = 3_000;
-    
+    private static Lazy<SingleInitializationSingleton> _instance =
+        new(() => new SingleInitializationSingleton()); 
     public int Delay { get; }
 
     private SingleInitializationSingleton(int delay = DefaultDelay)
@@ -22,14 +25,29 @@ public class SingleInitializationSingleton
 
     internal static void Reset()
     {
-        throw new NotImplementedException();
+        if (!_isInitialized)
+            return;
+        lock (Locker)
+        {
+            if (!_isInitialized)
+                return;
+            _instance = new Lazy<SingleInitializationSingleton>
+                (() => new SingleInitializationSingleton());
+            _isInitialized = false;
+        }
     }
 
     public static void Initialize(int delay)
     {
-        throw new NotImplementedException();
+        if (_isInitialized)
+            throw new InvalidOperationException("Singleton has been already initialized");
+        lock (Locker)
+        {
+            if (_isInitialized)
+                throw new InvalidOperationException("Singleton has been already initialized");
+            _isInitialized = true;
+            _instance = new Lazy<SingleInitializationSingleton>
+                (() => new SingleInitializationSingleton(delay));
+        }
     }
-
-    public static SingleInitializationSingleton Instance => throw new NotImplementedException();
-
 }
