6c2f7cbfebbd4cbac7528d91b6db2b1c2c04e765
selezen4ever <apdasd@mail.ru>
2022-10-31 18:47:38 +0300

    fix tests

--- Homework8/Hw8.Tests/CalculatorTests.cs
+++ Homework8/Hw8.Tests/CalculatorTests.cs
@@ -6,17 +6,16 @@ namespace Hw8.Tests;
 
 public class CalculatorTests
 {
+    private readonly ICalculator _calculator = new Calculator.Calculator();
+    
     [Theory]
     [InlineData(1, 2, 3)]
     [InlineData(-5.5, 2, -3.5)]
     [InlineData(10, 24.3, 34.3)]
     public void Plus_TwoNumbers_ReturnSum(double val1, double val2, double expResult)
     {
-        //arrange
-        ICalculator calculator = null;
-
         //act
-        var actual = calculator.Plus(val1, val2);
+        var actual = _calculator.Plus(val1, val2);
 
         //assert
         Assert.Equal(actual, expResult);
@@ -28,11 +27,8 @@ public class CalculatorTests
     [InlineData(10, -24.3, 34.3)]
     public void Minus_TwoNumbers_ReturnDiff(double val1, double val2, double expResult)
     {
-        //arrange
-        ICalculator calculator = null;
-
         //act
-        var actual = calculator.Minus(val1, val2);
+        var actual = _calculator.Minus(val1, val2);
 
         //assert
         Assert.Equal(actual, expResult);
@@ -44,11 +40,8 @@ public class CalculatorTests
     [InlineData(0, 24.3, 0)]
     public void Multiply_TwoNumbers_ReturnMultiplication(double val1, double val2, double expResult)
     {
-        //arrange
-        ICalculator calculator = null;
-
         //act
-        var actual = calculator.Multiply(val1, val2);
+        var actual = _calculator.Multiply(val1, val2);
 
         //assert
         Assert.Equal(actual, expResult);
@@ -59,11 +52,8 @@ public class CalculatorTests
     [InlineData(-5, 2, -2.5)]
     public void Divide_TwoNumbers_ReturnQuotient(double val1, double val2, double expResult)
     {
-        //arrange
-        ICalculator calculator = null;
-
         //act
-        var actual = calculator.Divide(val1, val2);
+        var actual = _calculator.Divide(val1, val2);
 
         //assert
         Assert.Equal(actual, expResult);
@@ -72,9 +62,7 @@ public class CalculatorTests
     [Fact]
     public void DivideByZero_ThrowsInvalidoperationException()
     {
-        ICalculator calculator = null;
-
         //act + assert
-        Assert.Throws<InvalidOperationException>(() => { calculator.Divide(1, 0); });
+        Assert.Throws<InvalidOperationException>(() => { _calculator.Divide(1, 0); });
     }
 }
